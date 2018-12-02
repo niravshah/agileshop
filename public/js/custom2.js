3a4b1eb27b340e0754c00d5263b2ce94f12440e2
shahnirav <shah.nirav@me.com>
2018-12-02 22:39:20 +0000

    M

--- public/js/custom2.js
+++ public/js/custom2.js
@@ -22,9 +22,6 @@
 
         var website_name = 'AgileShop.com';
 
-        var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
-        var elements = stripe.elements();
-
         $(':checkbox').radiocheck();
 
         $('#slide2').hide();
@@ -33,7 +30,6 @@
         $('#removeBrandingCheckout').hide();
         $('#professionalPrintCheckout').hide();
 
-        createStripeCheckoutForm();
         renderDisplayCanvas(dCanvas, dCanvasBg, dCanvasLogo);
 
         $('.slick-carousel').slick({
@@ -90,8 +86,8 @@
         });
 
         $('#removeBranding').click(function (e) {
-            $('#slide2').hide();
             generateDeck(true);
+            $('#slide2').hide();
             $('#checkout').show();
             $('#freeCheckout').hide();
             $('#professionalPrintCheckout').hide();
@@ -105,6 +101,8 @@
             $('#freeCheckout').hide();
             $('#removeBrandingCheckout').hide();
             $('#professionalPrintCheckout').show();
+            createStripeCheckoutForm();
+
         });
 
         $("form[name='freeCheckoutForm']").validate({
@@ -499,6 +497,10 @@
         }
 
         function createStripeCheckoutForm() {
+
+            var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+            var elements = stripe.elements();
+
             var style = {
                 base: {
                     fontSize: '16px',
@@ -508,6 +510,44 @@
 
             var card = elements.create('card', {style: style});
             card.mount('#card-element');
+
+            card.addEventListener('change', function (event) {
+                var displayError = document.getElementById('card-errors');
+                if (event.error) {
+                    displayError.textContent = event.error.message;
+                } else {
+                    displayError.textContent = '';
+                }
+            });
+
+            var form = document.getElementById('payment-form');
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
+
+                stripe.createToken(card).then(function (result) {
+                    if (result.error) {
+                        // Inform the customer that there was an error.
+                        var errorElement = document.getElementById('card-errors');
+                        errorElement.textContent = result.error.message;
+                    } else {
+                        // Send the token to your server.
+                        stripeTokenHandler(result.token);
+                    }
+                });
+            });
+        }
+
+        function stripeTokenHandler(token) {
+            // Insert the token ID into the form so it gets submitted to the server
+            var form = document.getElementById('payment-form');
+            var hiddenInput = document.createElement('input');
+            hiddenInput.setAttribute('type', 'hidden');
+            hiddenInput.setAttribute('name', 'stripeToken');
+            hiddenInput.setAttribute('value', token.id);
+            form.appendChild(hiddenInput);
+
+            // Submit the form
+            form.submit();
         }
 
     });
